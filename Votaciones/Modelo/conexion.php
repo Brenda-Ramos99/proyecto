48eee5e6c59d8a2865c5c2c70245714a978dd35e
Brenda Ramos <Brenda Ramos@BRENDA-RAMOS>
2019-05-13 19:27:55 -0600

    se modifico un archivo de la conexion a base de datos

--- Votaciones/Modelo/conexion.php
+++ Votaciones/Modelo/conexion.php
@@ -4,7 +4,7 @@ require 'parametros.php';
 class Conexion {
     protected $con;
         function __construct() {
-            $this->con=new mysqli(SERVER,USER,PASSWORD,VOTACIONES);
+            $this->con=new mysqli(SERVER,USER,PASSWORD,BASE);
             $this->con->set_charset(CHAR);
         
     }
