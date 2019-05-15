8674bdd3ef606ca3186255fe6de1595786617004
Brenda Ramos <Brenda Ramos@BRENDA-RAMOS>
2019-05-14 22:07:41 -0600

    se modificaron los links de busqueda

--- Votaciones/Modelo/DaoCiudadano.php
+++ Votaciones/Modelo/DaoCiudadano.php
@@ -5,19 +5,20 @@ if(isset($_POST['buscar']))
 ?>
    <!-- el resultado de la búsqueda lo encapsularemos en un tabla -->
    <meta charset="UTF-8">
-   <table width="100%" border="2px" align="center" cellpadding="1" cellspacing="1" lang="es">
+   
+   <table width="80%" border="1px" align="center" cellpadding="1" cellspacing="1" lang="es">
        <tr width="100" align="center">
        <strong><h1><center>Votaciones Presidenciales 2019</center></h1></strong>
        </tr>
-       <tr width="100" align="center">
+       <tr width="25" align="center">
        <strong><h3><center>Boleta de busqueda de Centro de Votación</center></h3></strong>
        </tr>
        <tr>
             <!--creamos los títulos de nuestras columnas  -->
-            <td width="100" align="center"><strong>DUI</strong></td>
-            <td width="100" align="center"><strong>Nombre de Centro de Votaciones</strong></td>
-            <td width="100" align="center"><strong>Nombre</strong></td>
-            <td width="100" align="center"><strong>Apellidos</strong></td>
+            <td width="25" align="center"><strong>DUI</strong></td>
+            <td width="25" align="center"><strong>Nombre de Centro de Votaciones</strong></td>
+            <td width="25" align="center"><strong>Nombre</strong></td>
+            <td width="25" align="center"><strong>Apellidos</strong></td>
        </tr> 
        <?php
        //obtenemos la información introducida anteriormente desde nuestro buscador PHP
@@ -39,11 +40,15 @@ if(isset($_POST['buscar']))
        } //fin blucle
     ?>
     </table>
-   <!-- <div id="madeby">
-        <span>
-            <a href="../Vistas/Buscar.php"><h3>Volver atras</h3></a>
-        </span>
-    </div> -->
+  
     <?php
 } // fin if 
+
 ?>
+ <div id="madeby">
+        <span>
+            <br><br>
+            <input type="submit"  name="Imprimir" value="Imprimir boleta" onclick="javascript:window.print()"> 
+            <a href="../Vistas/Buscar.php"><h3>Volver atras</h3></a>
+        </span>
+    </div>
